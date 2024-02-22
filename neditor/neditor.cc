a8ee725a7e528863b9fed45371b216229e2a0436
xundaoxd <xundaoxd@outlook.com>
2024-02-22 19:12:48 +0800

    reset imgui

--- neditor/neditor.cc
+++ neditor/neditor.cc
@@ -6,48 +6,11 @@
 
 #include "GLFW/glfw3.h" // Will drag system OpenGL headers
 
-#include "neditor/ImNodes.h"
-#include "neditor/ImNodesEz.h"
-
 static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-void DoDraw() {
-  if (ImGui::Begin("ImNodes", nullptr,
-                   ImGuiWindowFlags_NoScrollbar |
-                       ImGuiWindowFlags_NoScrollWithMouse)) {
-    ImNodes::Ez::BeginCanvas();
-
-    struct Node {
-      ImVec2 pos{};
-      bool selected{};
-      ImNodes::Ez::SlotInfo inputs[1];
-      ImNodes::Ez::SlotInfo outputs[1];
-    };
-
-    static Node nodes[3] = {
-        {{50, 100}, false, {{"In", 1}}, {{"Out", 1}}},
-        {{250, 50}, false, {{"In", 1}}, {{"Out", 1}}},
-        {{250, 100}, false, {{"In", 1}}, {{"Out", 1}}},
-    };
-
-    for (Node &node : nodes) {
-      if (ImNodes::Ez::BeginNode(&node, "Node Title", &node.pos,
-                                 &node.selected)) {
-        ImNodes::Ez::InputSlots(node.inputs, 1);
-        ImNodes::Ez::OutputSlots(node.outputs, 1);
-        ImNodes::Ez::EndNode();
-      }
-    }
-
-    ImNodes::Connection(&nodes[1], "In", &nodes[0], "Out");
-    ImNodes::Connection(&nodes[2], "In", &nodes[0], "Out");
-
-    ImNodes::Ez::EndCanvas();
-  }
-  ImGui::End();
-}
+void DoDraw() { ImGui::ShowDemoWindow(); }
 
 int main(int argc, char *argv[]) {
   (void)argc;
@@ -66,27 +29,30 @@ int main(int argc, char *argv[]) {
   }
   glfwMakeContextCurrent(window);
 
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard |
-                    ImGuiConfigFlags_NavEnableGamepad |
-                    ImGuiConfigFlags_DockingEnable;
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init();
-
-  auto imnodes_ctx = ImNodes::Ez::CreateContext();
+  {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard |
+                      ImGuiConfigFlags_NavEnableGamepad |
+                      ImGuiConfigFlags_DockingEnable;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+  }
 
   while (!glfwWindowShouldClose(window)) {
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    int width, height;
-    glfwGetWindowSize(window, &width, &height);
-    ImGui::SetNextWindowSize(
-        ImVec2(width, height)); // ensures ImGui fits the GLFW window
+    {
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+    }
+    {
+      ImGui::SetNextWindowPos(ImVec2(0, 0));
+      int width, height;
+      glfwGetWindowSize(window, &width, &height);
+      ImGui::SetNextWindowSize(
+          ImVec2(width, height)); // ensures ImGui fits the GLFW window
+    }
 
     DoDraw();
 
@@ -103,11 +69,11 @@ int main(int argc, char *argv[]) {
     glfwPollEvents();
   }
 
-  ImNodes::Ez::FreeContext(imnodes_ctx);
-
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
+  {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+  }
 
   glfwDestroyWindow(window);
   glfwTerminate();
