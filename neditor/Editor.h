3f4cfe880a1470e1a636832a778c8ce0ae585dcc
yinchaogao <yinchaogao@deeproute.ai>
2024-02-23 19:33:08 +0800

    update

--- neditor/Editor.h
+++ neditor/Editor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cmath>
 #include <iostream>
+#include <memory>
+#include <numeric>
 #include <vector>
 
 #include "imgui.h"
@@ -21,12 +23,37 @@ struct Node {
   std::string title;
   std::vector<std::string> in_slots;
   std::vector<std::string> out_slots;
+
+  Node(std::string title, std::vector<std::string> in_slots,
+       std::vector<std::string> out_slots)
+      : title(std::move(title)), in_slots(std::move(in_slots)),
+        out_slots(std::move(out_slots)) {}
+
+  Node(std::string title) : Node(std::move(title), {}, {}) {}
 };
 
 class NodeEditor {
+  static constexpr float padding = 6;
+  static constexpr float mark = 6;
+
+  struct NodeP {
+    std::unique_ptr<Node> node{nullptr};
+    ImVec2 pos{0, 0};
+
+    NodeP(std::unique_ptr<Node> node, ImVec2 pos)
+        : node(std::move(node)), pos(std::move(pos)) {}
+  };
+
   ImVec2 scrolling{0, 0};
+  std::vector<NodeP> nodes;
 
 public:
+  NodeEditor() {
+    // TODO: test data
+    nodes.emplace_back(std::make_unique<Node>("demo"), ImVec2{100, 100});
+    nodes.back().node->in_slots = {"in0", "in1"};
+    nodes.back().node->out_slots = {"out0", "out1"};
+  }
   void DrawMenuBar() {
     if (ImGui::BeginMenuBar()) {
       if (ImGui::BeginMenu("File")) {
@@ -101,8 +128,44 @@ public:
   //   }
   // }
 
+  // | <---padding---> elem <---padding--->|
   void DrawNodes() {
-    // TODO: impl
+    // ImGuiIO &io = ImGui::GetIO();
+    ImDrawList *draw_list = ImGui::GetWindowDrawList();
+    ImU32 white_col = ImGui::GetColorU32(IM_COL32(255, 255, 255, 255));
+
+    for (auto &node : nodes) {
+      ImVec2 node_p0 = node.pos;
+      ImVec2 cur_p0 = node_p0;
+
+      cur_p0 = cur_p0 + ImVec2{0, padding};
+      {
+        // draw title
+        ImVec2 title_sz = ImGui::CalcTextSize(node.node->title.c_str(),
+                                              node.node->title.c_str() +
+                                                  node.node->title.size());
+        ImVec2 title_p0 = cur_p0 + ImVec2{padding, padding};
+        ImVec2 title_p1 = title_p0 + title_sz;
+        draw_list->AddText(title_p0, white_col, node.node->title.c_str(),
+                           node.node->title.c_str() + node.node->title.size());
+
+        cur_p0 = ImVec2{cur_p0.x, title_p1.y};
+      }
+      {
+        // draw seperator
+        cur_p0 = cur_p0 + ImVec2{0, 10};
+      }
+      {
+        // draw slots
+        // |<---padding---> islot <---2 * padding---> oslot <---padding--->|
+        // islot: |mark <---padding---> slot|
+        // oslot: |slot <---padding---> mark|
+      }
+      {
+        // draw outline
+      }
+      // TODO: impl
+    }
   }
 
   void DrawConns() {
@@ -120,7 +183,7 @@ public:
     DrawNodes();
     DrawConns();
 
-    ImGui::ShowDemoWindow();
+    // ImGui::ShowDemoWindow();
 
     ImGui::End();
   }
