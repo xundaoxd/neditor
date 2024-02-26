801eaf9c6d81dd15a1b923cdc12752c52e7ceb4f
yinchaogao <yinchaogao@deeproute.ai>
2024-02-26 18:53:47 +0800

    demo

--- neditor/NodeEditor.h
+++ neditor/NodeEditor.h
@@ -63,7 +63,11 @@ public:
     }
   }
   void UpdateGraph() {
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     ImGui::Begin("NodeGraph");
+    ImGui::PopStyleVar(3);
     ngraph.Update();
     ImGui::End();
   }
