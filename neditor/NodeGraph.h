710d7431226ac0ecbc0046ed0c815cb0a62b09cb
xundaoxd <xundaoxd@outlook.com>
2024-03-02 17:07:10 +0800

    refactor

--- neditor/NodeGraph.h
+++ neditor/NodeGraph.h
@@ -9,6 +9,9 @@
 class NodeGraph {
   constexpr static float GRID_STEP{64.0f};
 
+  constexpr static ImU32 color_cbackground = IM_COL32(50, 50, 50, 255);
+  constexpr static ImU32 color_cforeground = IM_COL32(200, 200, 200, 40);
+
   ImVec2 canvas_p0;
   ImVec2 canvas_sz;
   ImVec2 scrolling{0.f, 0.f};
@@ -21,20 +24,17 @@ class NodeGraph {
     canvas_sz = ImGui::GetContentRegionAvail();
 
     draw_list->AddRectFilled(canvas_p0, canvas_p0 + canvas_sz,
-                             IM_COL32(50, 50, 50, 255));
-    draw_list->AddRect(canvas_p0, canvas_p0 + canvas_sz,
-                       IM_COL32(200, 200, 200, 40));
+                             color_cbackground);
+    draw_list->AddRect(canvas_p0, canvas_p0 + canvas_sz, color_cforeground);
     for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x;
          x += GRID_STEP) {
       draw_list->AddLine(canvas_p0 + ImVec2{x, 0},
-                         canvas_p0 + ImVec2{x, canvas_sz.y},
-                         IM_COL32(200, 200, 200, 40));
+                         canvas_p0 + ImVec2{x, canvas_sz.y}, color_cforeground);
     }
     for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y;
          y += GRID_STEP) {
       draw_list->AddLine(canvas_p0 + ImVec2{0, y},
-                         canvas_p0 + ImVec2{canvas_sz.x, y},
-                         IM_COL32(200, 200, 200, 40));
+                         canvas_p0 + ImVec2{canvas_sz.x, y}, color_cforeground);
     }
     ImGui::InvisibleButton("##", canvas_sz);
     if (ImGui::IsItemHovered() &&
@@ -56,6 +56,8 @@ public:
     ImGui::PopStyleVar(3);
 
     UpdateCanvas();
+    ImGui::PushClipRect(canvas_p0, canvas_p0 + canvas_sz, true);
+    ImGui::PopClipRect();
 
     ImGui::End();
   }
